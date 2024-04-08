238cb686f8037a969eeb74dddbd0182f42d9c829
Onedrican <v.bernardini@hispeed.ch>
2024-04-08 15:23:22 +0200

    first ausgabe sql

--- index.php
+++ index.php
@@ -8,15 +8,30 @@
 </head>
 <body>
     <?php
-        $servername = "localhost";
+        //Connection to the database
+        $servername = "127.0.0.1:3306";
         $username = "rundb";
         $password = "runpass";
         try {
             $conn = new PDO("mysql:host=$servername;dbname=books", $username, $password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             echo "Connected successfully";
         } catch(PDOException $e) {
             echo "Connection failed: " . $e->getMessage();
         }
+
+        //Query to get all the books    
+        $sql = "SELECT * FROM buecher";
+        $result = $conn->query($sql);
+        
+        if ($result->rowCount() > 0) {
+            // output data of each row
+            while($row = $result->fetch()) {
+              echo "id: " . $row["id"]. " - Kurztitel: " . $row["kurztitle"]. " - Author: " . $row["autor"]. "<br>";
+            }
+          } else {
+            echo "0 results";
+          }
     ?>
 </body>
 </html>
