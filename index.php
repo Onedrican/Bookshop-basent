de242f4eb33d80c8cbd9494aafe36372af6aff0a
Onedrican <v.bernardini@hispeed.ch>
2024-04-08 14:37:14 +0200

    connection sql server
    connection sql server

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
-gay shit
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $servername = "localhost";
+        $username = "rundb";
+        $password = "runpass";
+        try {
+            $conn = new PDO("mysql:host=$servername;dbname=books", $username, $password);
+            echo "Connected successfully";
+        } catch(PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+        }
+    ?>
+</body>
+</html>
